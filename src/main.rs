e595c37abd537e1fbd366cc1562bdbfa4eab0d50
Theepakorn Phayonrat <pottarrpongs@gmail.com>
2024-08-31 14:46:54 +0700

    Almost Finish

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use crossterm::{
-    cursor::{Hide, MoveTo, Show},
-    event::{self, EnableMouseCapture, KeyCode, KeyEvent},
-    execute,
-    style::{Color, Print, ResetColor, SetAttribute, SetForegroundColor},
+    cursor::{Hide, MoveTo},
+    event::{self, KeyCode, KeyEvent},
+    style::{Color, ResetColor, SetForegroundColor},
     terminal::{self, Clear, ClearType},
     ExecutableCommand,
 };
 use rand::Rng;
 use std::{
-    cmp::max_by_key, collections::HashSet, error::Error, io::{stdout, Write}, thread, time::{Duration, Instant}
+     io::{stdout, Write}, thread, time::{Duration, Instant}
 };
 
 enum Direction {
@@ -18,10 +17,10 @@ enum Direction {
     West
 }
 
-enum MeteorState {
-    Ground,
-    Falling
-}
+// enum MeteorState {
+//     Ground,
+//     Falling
+// }
 
 struct Player {
     alive: bool,
@@ -74,30 +73,43 @@ impl Player {
         }
     }
 
-    fn player_score(&mut self) {
+    fn add_score(&mut self) {
         self.score += 1;
     }
 }
 
+
 struct Meteor {
-    id: u8,
+    // id: u8,
     x: u16,
     y: u16,
-    state: MeteorState
+    // state: MeteorState
 }
 
 impl Meteor {
-    fn new(meteor_id: u8, max_x: u16, max_y: u16) -> Meteor {
+    // fn new(meteor_id: u8, max_x: u16, max_y: u16) -> Meteor {
+    fn new(max_x: u16, max_y: u16) -> Meteor {
         let mut rng = rand::thread_rng();
         let meteor_x = rng.gen_range(1..=max_x);
         let meteor_y = rng.gen_range(1..=max_y);
         Meteor {
-            id: meteor_id,
+            // id: meteor_id,
             x: meteor_x,
             y: meteor_y,
-            state: MeteorState::Falling
+            // state: MeteorState::Falling
         }
     }
+    // fn falling(&mut self, mut met_time: Instant) {
+    //     if met_time.elapsed() >= Duration::new(0, 0) && met_time.elapsed() <= Duration::new(1, 0) {
+    //         show_entity(self.x, self.y, "@", Color::White);
+    //     } else if met_time.elapsed() >= Duration::new(1, 0) && met_time.elapsed() <= Duration::new(2, 0) {
+    //         show_entity(self.x, self.y, "@", Color::Yellow);
+    //     } else if met_time.elapsed() >= Duration::new(2, 0) && met_time.elapsed() <= Duration::new(5, 0) {
+    //         show_entity(self.x, self.y, "@", Color::Red);
+    //     } else {}
+    //     met_time = Instant::now();
+    //     // thread::sleep(Duration::from_secs(3));
+    // }
 }
 
 fn show_entity(x: u16, y: u16, entity: &str, color: Color) {
@@ -112,34 +124,54 @@ fn main() {
     let mut stdout = stdout();
     stdout.execute(Hide).unwrap();
     terminal::enable_raw_mode().unwrap();
-
+    
     let mut player = Player::new();    
     let max_x: u16 = 40;
     let max_y: u16 = 45;
-    let start = Instant::now();
-
+    
+    let mut meteor_vec: Vec<Meteor> = Vec::new();
+    
+    let mut score_time = Instant::now();
+    let mut summon_met_time = Instant::now();
+    let mut met_time = Instant::now();
+    
     loop {
 
-        let elapsed = start.elapsed();
-        let elapsed_milli = elapsed.as_millis();
+        if player.alive == false || player.score == 60 {
+            break;
+        }
 
-        if elapsed_milli % 1000 == 0 {
-            player.score += 1;
+        if summon_met_time.elapsed() >= Duration::new(1, 0) {
+            meteor_vec.push(Meteor::new(max_x, max_y));
+            // meteor_vec.push(Meteor::new(meteor_vec.len() as u8 + 1, max_x, max_y));
+            summon_met_time = Instant::now();
         }
         
-        let mut meteor_vec: Vec<Meteor> = Vec::new();
-        if meteor_vec.len() < 10 && elapsed_milli % 100 == 0 {
-            meteor_vec.push(Meteor::new(meteor_vec.len() as u8 + 1, max_x, max_y))
-        }
-        show_entity(0, 0, &format!("Score: {}", player.score), Color::White);
         stdout.execute(Clear(ClearType::All)).unwrap();
         
-        // show_entity(0, 0, &format!("Score: {}", player.score), Color::White);
-        show_entity(player.x, player.y, "|", Color::Blue);
+        for met in &mut meteor_vec {
+            if player.x == met.x && player.y == met.y {
+                player.alive = false;
+            }
+            
+            if met_time.elapsed() >= Duration::new(0, 0) && met_time.elapsed() <= Duration::new(1, 0) {
+                show_entity(met.x, met.y, "@", Color::Red);
+            }
+            // } else if met_time.elapsed() >= Duration::new(1, 0) && met_time.elapsed() <= Duration::new(2, 0) {
+                //     show_entity(met.x, met.y, "@", Color::Yellow);
+                // } else if met_time.elapsed() >= Duration::new(2, 0) && met_time.elapsed() <= Duration::new(5, 0) {
+                    //     show_entity(met.x, met.y, "@", Color::Red);
+                    // } else {}
+                    met_time = Instant::now();
+                    
+                    // met.falling(met_time);
+                    // met_time = Instant::now();
+                }
+                
+                show_entity(0, 0, &format!("Score: {}", player.score), Color::White);
         
-        for i in &meteor_vec {
-            show_entity(i.x, i.y, "@", Color::Red);
-        }
+        show_entity(0, 0, &format!("Score: {}", player.score), Color::White);
+        show_entity(player.x, player.y, "|", Color::Blue);
         
         if let Ok(true) = event::poll(Duration::from_millis(1)) {
             if let Ok(event::Event::Key(KeyEvent { code, .. })) = event::read() {
@@ -161,9 +193,18 @@ fn main() {
                 }
             }
         }
-        // player.score += 1;
+        
+        
         stdout.flush().unwrap();
+        
+        if score_time.elapsed() >= Duration::new(1,0) {
+            player.add_score();
+            score_time = Instant::now();
+        }
     }
     thread::sleep(Duration::from_millis(1000));
+    stdout.execute(Clear(ClearType::All)).unwrap();
+    
+    // let start = Instant::now();
     
 }
